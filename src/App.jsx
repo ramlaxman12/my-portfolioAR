5999f82e72804f720068ce18921424ee250c9f49
ramlaxman12 <34443446+ramlaxman12@users.noreply.github.com>
2025-12-06 06:07:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import CTA from "./components/CTA";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Pricing from "./components/Pricing";
-import CTA from "./components/CTA";
 import Footer from './components/Footer';
 
 import "./App.css";
