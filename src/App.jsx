c21b7895e747ac31221cd248a0772cd9428be33a
ramlaxman12 <34443446+ramlaxman12@users.noreply.github.com>
2025-12-06 05:51:15 +0530

    Update App.jsx file moved CTA line

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
+import CTA from "./components/CTA";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Pricing from "./components/Pricing";
-import CTA from "./components/CTA";
 import Footer from './components/Footer';
 
 import "./App.css";
